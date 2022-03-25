71e0a407b7af5e5377352f7315a1c459e3f3bba6
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2022-03-25 16:24:11 -0500

    feat: CORS MiddleWare

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.user import user_router
 from .routes.template import template_router
 from .routes.message import message_router
@@ -8,6 +9,15 @@ app = FastAPI(
     description= "Backend to control Messages sended by Evergreen",
     version="1.0.1"
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
 app.include_router(template_router)
